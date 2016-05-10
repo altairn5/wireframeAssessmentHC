a5c99eab0d5ecaf6a0a4d3c5f16b9f3601faf035
Humberto Calero <altairn5@uga.edu>
2016-05-09 20:30:50 -0700

    process.env.port added

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.all('*', function (req, res) {
 });
 
 // listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.port || 3000), function() {
   console.log('server started');
-});
+}));
